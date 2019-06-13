1291aa31da68ae46b30233b63aec6ff3c31a1239
johnnynode <johnnynode@gmail.com>
2019-06-13 22:54:52 +0800

    update: movie detail page ui

--- contents/projects/code/movie/client/pages/movie/detail/detail.js
+++ contents/projects/code/movie/client/pages/movie/detail/detail.js
@@ -15,125 +15,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getProductList()
+    
   },
 
-  getProductList() {
-    wx.showLoading({
-      title: '商品数据加载中',
+  /* 打开actionsheet */
+  openActionsheet: function(e) {
+    wx.showActionSheet({
+      itemList: ['文字','音频'],
+      itemColor: '#007aff',
+      success(res) {
+        console.log(res.tapIndex);
+        console.log('.....');
+
+        // 跳转到编辑影评页面
+        wx.navigateTo({
+          url: '../../comments/list/list?type=' + res.tapIndex
+        });
+      }
     })
-    qcloud.request({
-      url: config.service.productList,
-      success: result => {
-        wx.hideLoading()
-
-        if (!result.data.code) {
-          this.setData({
-            productList: result.data.data
-          })
-        } else {
-          wx.showToast({
-            title: '商品数据加载失败',
-          })
-        }
-      },
-      fail: result => {
-        wx.hideLoading()
-        wx.showToast({
-          title: '商品数据加载失败',
-        })
-      }
-    });
-  },
-
-  addToTrolley(event) {
-    let productId = event.currentTarget.dataset.id
-    let productList = this.data.productList
-    let product
-
-    for (let i = 0, len = productList.length; i < len; i++) {
-      if (productList[i].id === productId) {
-        product = productList[i]
-        break
-      }
-    }
-
-    if (product) {
-      qcloud.request({
-        url: config.service.addTrolley,
-        login: true,
-        method: 'PUT',
-        data: product,
-        success: result => {
-          let data = result.data
-
-          if (!data.code) {
-            wx.showToast({
-              title: '已添加到购物车',
-            })
-          } else {
-            wx.showToast({
-              icon: 'none',
-              title: '添加到购物车失败',
-            })
-          }
-        },
-        fail: () => {
-          wx.showToast({
-            icon: 'none',
-            title: '添加到购物车失败',
-          })
-        }
-      })
-    }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    console.log(e.currentTarget.dataset['index']);
   }
 })
