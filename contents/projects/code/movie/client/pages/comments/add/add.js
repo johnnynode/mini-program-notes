0cf125661e09727c531cc3588290f57706b664d2
johnnynode <johnnynode@gmail.com>
2019-08-01 08:30:04 +0800

    update: movie project audio record and uploading

--- contents/projects/code/movie/client/pages/comments/add/add.js
+++ contents/projects/code/movie/client/pages/comments/add/add.js
@@ -4,9 +4,9 @@ const config = require('../../../config')
 const _ = require('../../../utils/util')
 const windowWidth = wx.getSystemInfoSync().windowWidth
 const app = getApp()
+const recorderManager = wx.getRecorderManager()
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -18,7 +18,11 @@ Page({
     },
     editObj: {},
     movieTopWidth: 0.3 * windowWidth,
-    movieTopHeight: 0.46 * windowWidth
+    movieTopHeight: 0.46 * windowWidth,
+    timestart: 0,
+    timing: 0,
+    tempShortAudio: '',
+    isRecording: false
   },
 
   /**
@@ -32,6 +36,8 @@ Page({
       image: options.image
     }
     this.setData({editObj})
+    // 监听 录音
+    this.rmListener()
   },
 
   // textarea 的输入监听
@@ -66,6 +72,95 @@ Page({
     wx.navigateTo({
       url: '../preview/preview?num=' + num + '&id=' + id + '&title=' + title + '&image=' + image + '&text=' + text + '&voice=' + voice
    })
+  },
+
+  // 录音说话
+  say(e) {
+    let timestart = e.timeStamp
+    // 配置信息
+    const options = {
+      duration: 60000,
+      sampleRate: 8000,
+      numberOfChannels: 1,
+      encodeBitRate: 26000,
+      format: 'mp3',
+      frameSize: 50
+    }
+    recorderManager.start(options) 
+    this.setData({
+      timestart,
+      isRecording: true
+     })
+  },
+
+  // 手指移除
+  sayOver(e) {
+    let timing = e.timeStamp - this.data.timestart;
+    this.setData({
+      timing,
+      isRecording: false
+    })
+    recorderManager.stop();
+  },
+
+  // 文件上传
+  uploadFile(tempFilePath, time, size) {
+    // loading 处理
+    wx.showLoading({
+      title: '上传中...',
+    })
+    wx.uploadFile({
+      url: config.service.uploadUrl,
+      filePath: tempFilePath,
+      name: 'file',
+      header: {
+        contentType: "multipart/form-data", // 按需求增加
+      },
+      formData: {
+        file: tempFilePath
+      },
+      success: (res) => {
+        if(!(res.statusCode === 200 && res.errMsg === 'uploadFile:ok')) {
+          wx.showToast({
+            icon: 'none',
+            title: '上传失败：' + res.errMsg,
+          })
+          return
+        }
+        wx.showToast({
+          title: '上传成功!'
+        })
+        // 成功获取结果后, 将结果拼接起来 path;time;size 组成新字符串传递
+        let data = JSON.parse(res.data)
+        let url = data.data.imgUrl;
+        this.setData({
+          commentObj: {
+            text: this.data.commentObj.text, // 保持原样
+            voice: url + ';' + time + ';' + size
+          },
+          tempShortAudio: url.substr(url.lastIndexOf('/') + 1, url.length - 1)
+        })
+      },
+      complete() {
+        wx.hideLoading() // 隐藏loading
+      }
+    })
+  },
+  
+  // 录音 监听
+  rmListener() {
+    // 开始时间监听处理
+    recorderManager.onError((res) => {
+      wx.showToast({
+        icon: 'none',
+        title: '录音失败：' + res.errMsg,
+      })
+    })
+
+    recorderManager.onStop((res) => {
+      console.log('文件开始上传!');
+      this.uploadFile(res.tempFilePath, res.duration, res.fileSize);
+    })
   },
 
   /**
